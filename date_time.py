6298b08875cefeb4dc5b33764c338f44f1909d20
Bekzod Buyukov <bek.buyukov@gmail.com>
2021-02-11 18:39:42 +0500

    datetime module experience

---
+++ date_time.py
@@ -0,0 +1,23 @@
+from contextlib import contextmanager
+from datetime import datetime
+
+
+# using context manager
+@contextmanager
+def tag(some_output):
+    """
+    Just to make it easier to look at a result on terminal
+    """
+    print(f'{some_output}')
+    yield
+    print(f'{some_output}')
+
+
+def main():
+    # printing date and time in default style
+    print(datetime.now())
+
+
+if __name__ == "__main__":
+    with tag('\n'):
+        main()
