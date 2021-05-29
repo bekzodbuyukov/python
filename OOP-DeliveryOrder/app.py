7e2f8fa4f4d4a6656d07af180bff8e97ff755b47
Bekzod Buyukov <bek.buyukov@gmail.com>
2021-05-29 10:18:32 +0700

    Add Task Solution and Mock Data

---
+++ OOP-DeliveryOrder/app.py
@@ -0,0 +1,32 @@
+import json
+from delivery_order import DeliveryOrder
+
+
+def start_process(user_input: str) -> None:
+    try:
+        file = open(file="mock_data.json", mode="r")
+    except FileNotFoundError:
+        print("Error: File not found.")
+    except OSError as error:
+        print(f"Error: Error {error.errno} {error.strerror}")
+    else:
+        contents = json.loads(file.read())
+        file.close()
+
+    orders = []
+    for content in contents["orders"]:
+        orders.append(DeliveryOrder(
+            content["distance"],
+            content["product_name"],
+            content["delivery_date"],
+            content["car_number"]
+        ))
+
+    for order in orders:
+        if order.is_delivery_date_similar(user_input):
+            print(order, end="\n\n")
+
+
+if __name__ == "__main__":
+    user_new_input = "07.07.2021"
+    start_process(user_new_input)
---
+++ OOP-DeliveryOrder/delivery_order.py
@@ -0,0 +1,22 @@
+import re
+
+
+class DeliveryOrder:
+    """ Class for managing Delivery Orders """
+    def __init__(self, distance: int, name: str, date: str, car_number: str) -> None:
+        self.distance = distance
+        self.product_name = name
+        self.delivery_date = date
+        self.car_number = car_number
+
+    def is_delivery_date_similar(self, comparing_date: str) -> bool:
+        return bool(
+            re.fullmatch(pattern=comparing_date, string=self.delivery_date)
+        )
+
+    def __repr__(self):
+        return f"Delivery Order details:" \
+               f"\nProduct name: {self.product_name}" \
+               f"\nDistance: {self.distance} km." \
+               f"\nDate: {self.delivery_date}" \
+               f"\nDelivery car: {self.car_number}"
